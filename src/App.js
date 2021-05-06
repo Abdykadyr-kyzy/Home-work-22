9a03974525afff35c0b58370162135ad6523bac8
Kanzada <abdykadyrkanzada222@gmail.com>
2021-05-06 15:54:21 +0600

    order

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {OrdersBasket} from './components/OrdersBasket'
+
+function App() {
+  return (
+    <div className="App">
+      <OrdersBasket/>
+    </div>
+  );
+}
+
+export default App;
